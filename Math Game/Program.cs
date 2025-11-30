4377caf55dbec8362fe9ef12b59ca4339d0be67e
leonardo <leonardomlouzas@gmail.com>
2025-11-30 17:22:52 -0300

    Add main menu

--- Math Game/Program.cs
+++ Math Game/Program.cs
@@ -1,4 +1,5 @@
-﻿string input = "";
+﻿string? input = "";
+const string EXIT_COMMAND = "EXIT";
 
 do
 {
@@ -8,8 +9,35 @@ do
     Console.WriteLine("1. Addition challenge");
     Console.WriteLine("2. Subtraction challenge");
     Console.WriteLine("3. Multiplication challenge");
-    Console.WriteLine("3. Division challenge");
+    Console.WriteLine("4. Division challenge");
     input = Console.ReadLine();
 
+    switch (input)
+    {
+        case "1":
+            Console.Clear();
+            Console.WriteLine("Addition challenges");
+            input = Console.ReadLine();
+            break;
+        case "2":
+            Console.Clear();
+            Console.WriteLine("Subtraction challenges");
+            input = Console.ReadLine();
+            break;
+        case "3":
+            Console.Clear();
+            Console.WriteLine("Multiplication challenges");
+            input = Console.ReadLine();
+            break;
+        case "4":
+            Console.Clear();
+            Console.WriteLine("Division challenges");
+            input = Console.ReadLine();
+            break;
+        default:
+            Console.WriteLine("Invalid option. Press ENTER to go back or type EXIT to exit the game");
+            input = Console.ReadLine();
+            break;
+    }
 
-} while (input != "");
+} while (!string.Equals(input, EXIT_COMMAND, StringComparison.OrdinalIgnoreCase));
