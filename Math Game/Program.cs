d728560ebba99ee37ed73b586e2a93558ccaf3b5
leona <leona@DEVELOPMENT>
2025-11-30 17:09:49 -0300

    Add project files.

---
+++ Math Game/Program.cs
@@ -0,0 +1,15 @@
+﻿string input = "";
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Welcome to the Math Game!");
+    Console.WriteLine("Choose an option:");
+    Console.WriteLine("1. Addition challenge");
+    Console.WriteLine("2. Subtraction challenge");
+    Console.WriteLine("3. Multiplication challenge");
+    Console.WriteLine("3. Division challenge");
+    input = Console.ReadLine();
+
+
+} while (input != "");
